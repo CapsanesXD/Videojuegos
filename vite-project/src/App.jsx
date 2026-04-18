4fa44c110e12cbb1058af8bd1f4a6459d4f4b34e
CapsanesXD <stivenvalencis@gmail.com>
2026-04-18 13:02:31 +0000

    mejora front

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,26 +1,19 @@
 import Header from './Components/Header';
-
 import Sidebar from './Components/Sidebar';
-
 import Footer from './Components/Footer';
- 
+
 export default function App() {
-
   return (
-<div className="dashboard">
-<Header />
-<div className="main-content">
-<Sidebar />
-<section className="content">
-<h2>Bienvenido al Dashboard de juegos.com</h2>
-<p>Este es el espacio principal de la aplicación.</p>
-</section>
-</div>
-<Footer />
-</div>
-
+    <div className="dashboard">
+      <Header />
+      <div className="main-layout">
+        <Sidebar />
+        <main className="content">
+          <h2>Bienvenido al Dashboard de juegos.com</h2>
+          <p>Este es el espacio principal de la aplicación.</p>
+        </main>
+      </div>
+      <Footer />
+    </div>
   );
-
 }
-
- 
--- vite-project/src/Components/Header.jsx
+++ vite-project/src/Components/Header.jsx
@@ -1,6 +1,6 @@
 import logo from '../assets/steaam.jpg';
 export default function Header() {
-  const title = "care chimba";
+  const title = "Juegos para ti";
   return (
     <header className="header">
       <img src={logo} alt="Logo" className="logo" />
